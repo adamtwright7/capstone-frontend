2ee32b7e5108fb6974d50f66019bf7521ec06094
stoicgit <118203310+stoicgit@users.noreply.github.com>
2023-03-01 14:57:48 -0500

    profile update
    what i have so far

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,83 +1,74 @@
 import React from "react";
+import "./ProfilePage.css";
 import { Link } from "react-router-dom";
 
 const ProfilePage = () => {
   return (
-    <div>
-      <div className="mainContainer bg-stone-900">
-        <div class="container mx-auto px-4 bg-stone-900">
-          <nav class="flex items-center justify-between flex-wrap bg-goldAccents p-6">
-            <div class="flex items-center flex-shrink-0 text-gray-600 mr-6">
-              <span class="font-semibold text-xl tracking-tight">
-                Plot Points
-              </span>
-            </div>
-            <div class="block lg:hidden">
-              <button class="flex items-center px-3 py-2 border rounded text-gray-600 border-gray-600 hover:text-white hover:border-white">
-                <svg
-                  class="fill-current h-3 w-3"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Menu</title>
-                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                </svg>
+    <div className="container">
+      <div className="header">
+        <h1>Plot Points</h1>
+        <div className="nav">
+          <a href="#">Home</a>
+          <a href="#">Profile</a>
+          <a href="#">Settings</a>
+          <a href="#">Logout</a>
+        </div>
+      </div>
+      <div className="main">
+        <div className="profile-info">
+          {/* <img src="profile-pic.png" alt="Profile Picture"> */}
+          <h2>Evil Adam</h2>
+          <p>Level 5 Wizard</p>
+          <p>Joined on January 1, 2022</p>
+          <p>
+            Bio: I am an evil wizard who seeks power and domination over all.
+          </p>
+        </div>
+        <div className="activity">
+          <h3>Recent Activity</h3>
+          <ul>
+            <li>
+              Completed quest: <a href="#">The Goblin Menace</a>
+            </li>
+            <li>
+              Received loot: <a href="#">+1 Longsword</a>
+            </li>
+            <li>
+              Defeated monster: <a href="#">Orc Warlord</a>
+            </li>
+          </ul>
+          <button className="room-button">Select Room</button>
+          <button className="create-room-button">Create Room</button>
+        </div>
+        <div className="settings">
+          <h3>Settings</h3>
+          <ul>
+            <li>
+              <button className="change-email-button">Change Email</button>
+            </li>
+            <li>
+              <button className="change-password-button">
+                Change Password
               </button>
-            </div>
-            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              <div class="text-sm lg:flex-grow">
-                <a
-                  href="#responsive-header"
-                  class="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-white mr-4"
-                >
-                  Rooms
-                </a>
-                <a
-                  href="#responsive-header"
-                  class="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-white mr-4"
-                ></a>
-                <a
-                  href="#responsive-header"
-                  class="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-white"
-                >
-                  Stater Sets
-                </a>
-              </div>
-              <div>
-                <a
-                  href="#"
-                  class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-                >
-                  Profile
-                </a>
-              </div>
-            </div>
-          </nav>
+            </li>
+          </ul>
         </div>
-        <nav class="bg-white shadow-lg py-4">
-          <div class="max-w-7xl mx-auto px-4">
-            <div class="flex justify-between">
-              <div class="font-bold text-xl">Your Profile Name</div>
-
-              <a
-                href="#"
-                class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full"
-              >
-                Create Room
-              </a>
-            </div>
-          </div>
-        </nav>
-
-        <div class="flex justify-center items-center h-screen">
-          <div class="bg-gray-200 p-8 rounded-lg shadow-xl">
-            <h1 class="text-3xl font-bold mb-4">Map Room</h1>
-            <p class="text-gray-700">
-              Welcome to the Map Room. Here you can explore and discover new
-              places.
-            </p>
-          </div>
+        <div className="recent-activity">
+          <h3>Recent Activity</h3>
+          <ul>
+            <li>
+              Played a game of Dungeons & Dragons with Jane and Joe on March 1,
+              2023
+            </li>
+            <li>
+              Created a new character for a Pathfinder game on February 28, 2023
+            </li>
+            <li>Joined a campaign for Call of Cthulhu on February 27, 2023</li>
+          </ul>
         </div>
+      </div>
+      <div className="footer">
+        <p>&copy; 2023 An average table</p>
       </div>
     </div>
   );
