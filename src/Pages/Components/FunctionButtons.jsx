69b457135832916425d8c5e76b9adbd3a66f5ab3
VinnyVecchio <vcv36199@gmail.com>
2023-03-02 15:10:41 -0500

    added functionality to room page

--- src/Pages/Components/FunctionButtons.jsx
+++ src/Pages/Components/FunctionButtons.jsx
@@ -3,22 +3,27 @@ import "./functionButtons.css";
 import { BsFillArrowDownCircleFill } from "react-icons/bs";
 import { MdLandscape } from "react-icons/md";
 import { RiHandCoinFill } from "react-icons/ri";
+import { useState } from "react";
+import { Scenes } from "./Scenes";
 
 export const FunctionButtons = () => {
+  const [showScenePopup, setshowScenePopup] = useState(false);
+
   return (
     <div className="functionMain">
+      {showScenePopup && <Scenes />}
       <div className="arrowImage">
         <p>
           <BsFillArrowDownCircleFill />
         </p>
       </div>
       <div className="bottomSection">
-        <p>
+        <button onClick={() => setshowScenePopup(true)}>
           <MdLandscape />
-        </p>
-        <p>
+        </button>
+        <button>
           <RiHandCoinFill />
-        </p>
+        </button>
       </div>
     </div>
   );
